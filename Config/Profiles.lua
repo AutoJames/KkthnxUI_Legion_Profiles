1a3cc72402217b80689f3ca87205095d722e76f7
James <autonamus@gmail.com>
2016-11-10 12:31:04 +0000

    Init

---
+++ Config/Profiles.lua
@@ -0,0 +1,70 @@
+local K, C, L = select(2, ...):unpack()
+
+-- CharacterName personal config
+if (K.Name == "CharacterName") and (K.Realm == "RealmName") then
+
+end
+
+-- CharacterName personal config
+if (K.Name == "Autonamus" or K.Name == "Aminel") and (K.Realm == "Illidan") then
+  C["General"]["CustomLagTolerance"] = true
+	C["General"]["TranslateMessage"] = false
+	C["General"]["WelcomeMessage"] = false
+
+	C["ActionBar"]["EquipBorder"] = true
+	C["ActionBar"]["RightBarsMouseover"] = false
+	C["ActionBar"]["StanceBarMou5seover"] = false
+	C["ActionBar"]["ToggleMode"] = false
+
+	C["Announcements"]["PullCountdown"] = false
+
+	C["Automation"]["AutoCollapse"] = false
+	C["Automation"]["AutoInvite"] = true
+	C["Automation"]["LoggingCombat"] = true
+	C["Automation"]["TabBinder"] = true
+	C["Automation"]["ScreenShot"] = true
+
+	C["Bags"]["Enable"] = false
+
+	C["Blizzard"]["ColorTextures"] = true
+	C["Blizzard"]["ReputationGain"] = true
+
+	--C["DataBars"]
+
+	C["Auras"]["CastBy"] = true
+
+	C["Chat"]["TabsMouseover"] = false
+	C["Chat"]["TabsOutline"] = true
+
+	C["DataText"]["Location"] = false
+	C["DataText"]["LocalTime"] = false
+  --C["DataText"]["BottomBar"] = false --Disabled till after beta
+
+	--C["Cooldown"]
+
+	C["Filger"]["ShowTooltip"] = true
+
+	--C["Loot"]
+	--C["Minimap"]
+
+	C["Misc"]["AFKCamera"] = true
+	C["Misc"]["AlreadyKnown"] = true
+	C["Misc"]["BGSpam"] = true
+	C["Misc"]["ColorPicker"] = true
+	C["Misc"]["MoveBlizzard"] = true
+
+	--C["Nameplates"]
+	--C["PulseCD"]
+
+	C["Skins"]["DBM"] = true
+	C["Skins"]["DBMMove"] = true
+	C["Skins"]["MinimapButtons"] = true
+	C["Skins"]["Skada"] = true
+	C["Skins"]["WeakAuras"] = true
+
+	--C["Tooltip"]
+	--C["Unitframe"]
+	--C["Partyframe"]
+	--C["Raidframe"]
+	--C["Worldmap"]
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+Backup and storage location for my **KkthnxUI** Profiles.lua file and the **FreeFileSync** batch file I use for updating it easier.
+
+### Credits
+* Kkthnx - [Kkthnx Legion UI](https://github.com/Kkthnx/KkthnxUI_Legion)
+* FreeFileSync - http://www.freefilesync.org
