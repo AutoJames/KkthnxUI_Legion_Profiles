bee45e61b5a4499b05ce117d4a8550e08e0aa40e
James <autonamus@gmail.com>
2016-12-11 17:44:14 +0000

    Update

--- Config/Profiles.lua
+++ Config/Profiles.lua
@@ -1,4 +1,7 @@
-local K, C, L = select(2, ...):unpack()
+local K, C, L = unpack(select(2, ...))
+
+-- Wow API
+local SetCVar = SetCVar
 
 -- CharacterName personal config
 if (K.Name == "CharacterName") and (K.Realm == "RealmName") then
@@ -7,74 +10,74 @@ end
 
 -- CharacterName personal config
 if (K.Name == "Autonamus" or K.Name == "Aminel") and (K.Realm == "Illidan") or (K.Name == "Téapot" or K.Name == "Amiinell") and (K.Realm == "Blackrock") then
-  C["General"]["CustomLagTolerance"] = true
-	C["General"]["TranslateMessage"] = false
-	C["General"]["WelcomeMessage"] = false
-  C["General"]["QuestSounds"] = false
+  C["General"].CustomLagTolerance = true
+	C["General"].TranslateMessage = false
+	C["General"].WelcomeMessage = false
+  C["General"].QuestSounds = false
 
-	C["ActionBar"]["EquipBorder"] = true
-	C["ActionBar"]["RightBarsMouseover"] = false
-	C["ActionBar"]["StanceBarMouseover"] = false
-	C["ActionBar"]["ToggleMode"] = false
+	C["ActionBar"].EquipBorder = true
+	C["ActionBar"].RightBarsMouseover = false
+	C["ActionBar"].StanceBarMouseover = false
+	C["ActionBar"].ToggleMode = false
 
-	C["Announcements"]["PullCountdown"] = false
+	C["Announcements"].PullCountdown = false
 
-	C["Automation"]["AutoCollapse"] = false
-	C["Automation"]["AutoInvite"] = true
-	C["Automation"]["LoggingCombat"] = true
-	C["Automation"]["TabBinder"] = true
-	C["Automation"]["ScreenShot"] = true
+	C["Automation"].AutoCollapse = false
+	C["Automation"].AutoInvite = true
+	C["Automation"].LoggingCombat = true
+	C["Automation"].TabBinder = true
+	C["Automation"].ScreenShot = true
 
-	C["Bags"]["Enable"] = false
+	C["Bags"].Enable = false
 
-	C["Blizzard"]["ColorTextures"] = true
-	C["Blizzard"]["ReputationGain"] = true
+	C["Blizzard"].ColorTextures = true
+	C["Blizzard"].ReputationGain = true
 
 	--C["DataBars"]
 
-	C["Auras"]["CastBy"] = true
+	C["Auras"].CastBy = true
 
-	C["Chat"]["TabsMouseover"] = false
-	C["Chat"]["TabsOutline"] = true
+	C["Chat"].TabsMouseover = false
+	C["Chat"].TabsOutline = true
 
-	C["DataText"]["Location"] = false
-	C["DataText"]["LocalTime"] = false
-  C["DataText"]["BottomBar"] = false --Disabled till after beta
+	C["DataText"].Location = true
+	C["DataText"].LocalTime = false
+  C["DataText"].BottomBar = false --Disabled till after beta
 
 	--C["Cooldown"]
 
-	C["Filger"]["ShowTooltip"] = true
+	C["Filger"].ShowTooltip = true
 
 	--C["Loot"]
 	--C["Minimap"]
 
-	C["Misc"]["AFKCamera"] = true
-	C["Misc"]["AlreadyKnown"] = true
-	C["Misc"]["BGSpam"] = true
-	C["Misc"]["ColorPicker"] = true
-	C["Misc"]["MoveBlizzard"] = true
-  C["Misc"]["Combat"] = false
+	C["Misc"].AFKCamera = true
+	C["Misc"].AlreadyKnown = true
+	C["Misc"].BGSpam = true
+	C["Misc"].ColorPicker = true
+	C["Misc"].MoveBlizzard = true
+  C["Misc"].Combat = false
 
 	--C["Nameplates"]
 	--C["PulseCD"]
 
-  C["RaidCD"]["Enable"] = true
-  C["RaidCD"]["Height"] = 15
-  C["RaidCD"]["Width"] = 186
-  C["RaidCD"]["UpWards"] = false
-  C["RaidCD"]["Expiration"] = false
-  C["RaidCD"]["ShowSelf"] = true
-  C["RaidCD"]["ShowIcon"] = true
-  C["RaidCD"]["ShowInRaid"] = true
-  C["RaidCD"]["ShowInParty"] = true
-  C["RaidCD"]["ShowInArena"] = true
+  C["RaidCD"].Enable = true
+  C["RaidCD"].Height = 15
+  C["RaidCD"].Width = 186
+  C["RaidCD"].UpWards = false
+  C["RaidCD"].Expiration = false
+  C["RaidCD"].ShowSelf = true
+  C["RaidCD"].ShowIcon = true
+  C["RaidCD"].ShowInRaid = true
+  C["RaidCD"].ShowInParty = true
+  C["RaidCD"].ShowInArena = true
 
-	C["Skins"]["DBM"] = true
-	C["Skins"]["DBMMove"] = true
-	C["Skins"]["MinimapButtons"] = true
-	C["Skins"]["Skada"] = true
-  C["Skins"]["Recount"] = true
-	C["Skins"]["WeakAuras"] = true
+	C["Skins"].DBM = true
+	C["Skins"].DBMMove = true
+	C["Skins"].MinimapButtons = true
+	C["Skins"].Skada = true
+  C["Skins"].Recount = true
+	C["Skins"].WeakAuras = true
 
 	--C["Tooltip"]
 	--C["Unitframe"]
@@ -83,5 +86,5 @@ if (K.Name == "Autonamus" or K.Name == "Aminel") and (K.Realm == "Illidan") or (
 	--C["Worldmap"]
 
   --MOVERS
-  C["Position"]["UnitFrames"]["PlayerCastbar"] = {"BOTTOM", "ActionBarAnchor", "BOTTOM", 2.9, 169}
+  C["Position"].UnitFrames.PlayerCastbar = {"BOTTOM", "ActionBarAnchor", "BOTTOM", 2.9, 169}
 end
