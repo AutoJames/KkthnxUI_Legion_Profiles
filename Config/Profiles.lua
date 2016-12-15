01dffa3bd0b4a81ec1e8cd23a6b83d5dba0dc977
James <autonamus@gmail.com>
2016-12-15 18:37:56 +0000

    Cleanup

--- Config/Profiles.lua
+++ Config/Profiles.lua
@@ -2,7 +2,6 @@ local K, C, L = unpack(select(2, ...))
 
 -- Wow API
 local SetCVar = SetCVar
-
 	C["General"].CustomLagTolerance = true
 	C["General"].TranslateMessage = false
 	C["General"].WelcomeMessage = false
@@ -26,18 +25,13 @@ local SetCVar = SetCVar
 	C["DataText"].Location = true
 	C["DataText"].LocalTime = false
 	C["DataText"].BottomBar = false
-	--C["Cooldown"]
 	C["Filger"].ShowTooltip = true
-	--C["Loot"]
-	--C["Minimap"]
 	C["Misc"].AFKCamera = true
 	C["Misc"].AlreadyKnown = true
 	C["Misc"].BGSpam = true
 	C["Misc"].ColorPicker = true
 	C["Misc"].MoveBlizzard = true
 	C["Misc"].Combat = false
-	--C["Nameplates"]
-	--C["PulseCD"]
 	C["RaidCD"].Enable = true
 	C["RaidCD"].Height = 15
 	C["RaidCD"].Width = 186
@@ -54,11 +48,6 @@ local SetCVar = SetCVar
 	C["Skins"].Skada = true
 	C["Skins"].Recount = true
 	C["Skins"].WeakAuras = true
-	--C["Tooltip"]
-	--C["Unitframe"]
-	--C["Partyframe"]
-	--C["Raidframe"]
-	--C["Worldmap"]
 
 	--MOVERS
 	C["Position"].UnitFrames.PlayerCastbar = {"BOTTOM", "ActionBarAnchor", "BOTTOM", 2.9, 169}
